a01423c45e472e8d78f434fcb9013e5855057461
Evan Summers <evans@saratoga.co.za>
2015-04-27 11:14:50 +0200

    update

--- services/postService.js
+++ services/postService.js
@@ -6,6 +6,9 @@ const postService = {
    find: function(id, cb) {
       redis.hgetall('post:dict:' + id, cb);
    },
+   exists: function(id, cb) {
+      redis.sismember('post:set', id, cb);
+   },
    latest: function(count, cb) {
       redis.lrange('post:list', 0, count, cb);
    },
